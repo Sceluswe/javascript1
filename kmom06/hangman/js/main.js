afe7806f0169120d61e5df90b58bcb96bbaac2ad
Sceluswe <emilcbm@gmail.com>
2017-06-21 23:24:46 +0200

    cleaned up code, discovered and fixed bug

--- kmom06/hangman/js/main.js
+++ kmom06/hangman/js/main.js
@@ -6,9 +6,18 @@
             id: "gameText",
             text: Letters.showPressed()
         });
- 
+
         console.log(Letters.showPressed());
 
         elem.appendChild(textElem);
+
+
+        var textElem = Elemu.create("p", {
+            id: "gameText",
+            text: Letters.showPressedCounter()
+        });
+
+        console.log(Letters.showPressedCounter());
+
     });
 })();
