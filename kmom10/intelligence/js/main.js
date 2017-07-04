8fb0213c656659943ed0cda2368bb42098e04f5a
Sceluswe <emilcbm@gmail.com>
2017-07-04 22:29:52 +0200

    added elemu.js: module with utilty functions and set an eventListener for the start-button

--- kmom10/intelligence/js/main.js
+++ kmom10/intelligence/js/main.js
@@ -2,6 +2,12 @@
     'use strict';
 
     // var myContent = document.getElementById('content');
+    Elemu.select(".startbutton", function (elem) {
+        elem.addEventListener("click", function () {
+            // Start the test. 
+            console.log("starting test!");
+        });
+    });
 
     console.log('Sandbox is ready!');
 })();
