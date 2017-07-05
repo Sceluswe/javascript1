07de38c039fd8d520ced13e94a29d93342170273
Sceluswe <emilcbm@gmail.com>
2017-07-05 21:46:51 +0200

    added createQuestions()

--- kmom10/intelligence/js/questions.js
+++ kmom10/intelligence/js/questions.js
@@ -1,3 +1,25 @@
-window.questions = (function () {
+window.Questions = (function () {
     // 1x2 module.
+
+    function createQuestion(question, answers, correctAnswer) {
+        Elemu.select(".content", function (elem) {
+            //  Create question node.
+            var questionNode = Elemu.create("p", {
+                classList: ["question"],
+                text: question
+            });
+
+            elem.appendChild(questionNode);
+        });
+    }
+
+    return {
+        "start": function () {
+            createQuestion(
+                "Vad händer med ett russin om du lägger det i ett glas med Champagne?",
+                ["Det flyter", "Det sjunker", "Det åker upp och ner"],
+                2
+            );
+        },
+    };
 })();
