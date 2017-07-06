f3b26ddbc3a6cd49dfe4a6bc833326bbd85e1d4d
Sceluswe <emilcbm@gmail.com>
2017-07-06 22:41:19 +0200

    added event handler to startButton

--- kmom10/intelligence/js/test.js
+++ kmom10/intelligence/js/test.js
@@ -1,10 +1,15 @@
 window.Test = (function () {
-    // Remove welcome screen.
-    window.Elemu.select(".content", function (elem) {
-        while (elem.firstChild) {
-            elem.removeChild(elem.firstChild);
-        }
+    window.Elemu.select(".startButton", function (elem) {
+        elem.addEventListener("click", function () {
+            // Remove welcome screen.
+            window.Elemu.select(".content", function (elem) {
+                while (elem.firstChild) {
+                    elem.removeChild(elem.firstChild);
+                }
+            });
+
+            window.Questions.start();
+        });
     });
 
-    window.Questions.start();
 })();
