6280f1fcd054f4d4fbe5c5626d8ca512682217a4
Sceluswe <emilcbm@gmail.com>
2017-07-06 23:59:53 +0200

    added explanator comment

--- kmom10/intelligence/js/test.js
+++ kmom10/intelligence/js/test.js
@@ -1,4 +1,5 @@
 window.Test = (function () {
+    // Add eventListener that starts the test if the user presses the button.
     window.Elemu.select(".startButton", function (elem) {
         elem.addEventListener("click", function () {
             // Remove welcome screen.
