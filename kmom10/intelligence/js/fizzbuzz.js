332ce2ca6f479a53feaf7f3dd66c0928a89925d3
Sceluswe <emilcbm@gmail.com>
2017-08-11 15:57:03 +0200

    added module fizzbuzz.js

---
+++ kmom10/intelligence/js/fizzbuzz.js
@@ -0,0 +1,18 @@
+window.Fizzbuzz = (function() {
+	// A module with a fizzbuzz question.
+	var sequence = "1, 2, Fizz, 4, Buzz, Fizz, 7, 8, Fizz, Buzz, 11, Fizz, 13, 14, Fizz Buzz, 16, 17";
+	var answer = "Fizz";
+
+	var fizzbuzz = {
+		"start": function (parentNode, callbackParam) {
+			// Start the test.
+			window.Elemu.select(parentNode, function (elem) {
+			});
+		}
+
+		"reset": function () {
+			// Reset the test and do it over again.
+		}
+	};
+	return fizzbuzz;
+}());
