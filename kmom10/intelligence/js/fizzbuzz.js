407f5045c6f457c5bb50ba2acc463548ae0d225e
Sceluswe <emilcbm@gmail.com>
2017-08-12 22:42:11 +0200

    added randomize function to fizzbuzz.js

--- kmom10/intelligence/js/fizzbuzz.js
+++ kmom10/intelligence/js/fizzbuzz.js
@@ -6,6 +6,14 @@ window.FizzBuzz = (function() {
 	var correctAnswer = "Fizz";
 	var points = 0;
 
+	/**
+	* Returns a random integer between min (inclusive) and max (inclusive)
+	* Using Math.round() will give you a non-uniform distribution!
+	*/
+	function getRandomInt(min, max) {
+		return Math.floor(Math.random() * (max - min + 1)) + min;
+	}
+
 	var fizzbuzz = {
 		"getPoints": function () {
 			return points;
