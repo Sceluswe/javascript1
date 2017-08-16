3738e273fc2a18964dbf9a3a802180d3fb9c4fdf
Sceluswe <emilcbm@gmail.com>
2017-08-16 23:01:43 +0200

    made memory.js more concrete. flag creation with createFlagDiv() and duplication with Array.concat(array), removed implicit and unnecessaryElement object

--- kmom10/intelligence/js/memory.js
+++ kmom10/intelligence/js/memory.js
@@ -1,84 +1,6 @@
 window.Memory = (function(){
 	'use strict';
 
-	function createFlagDiv(classes) {
-		var parentNode = window.Elemu.create("div", {classList: [className[0]]});
-		// Remove the first class index since that's the parent.
-		classes.shift();
-
-		// Apply all other classes children of parent.
-		forEach(function (item) {
-			parentNode.appendChild(window.Elemu.create("div", {classList: [item]}));
-		});
-	}
-
-	// HTML code for flags.
-	var germanFlag = createFlagDiv(["german", "ger-black-top", "ger-red-middle"]);
-	var jamaicaFlag = createFlagDiv(["jamaica", "jam-triangle-yellow-right", "jam-triangle-yellow-left", "jam-triangle-black-left", "jam-triangle-black-right"]);
-	var swedishFlag = createFlagDiv(["sweden", "swe-yellow-top", "swe-yellow-middle"]);
-	var southAfricanFlag = createFlagDiv(["sa-square-red-top", "sa-square-white-middle", "sa-square-green-middle", "sa-triangle-white", "sa-triangle-green", "sa-triangle-yellow", "sa-triangle-black"]);
-	var norwegianFlag = createFlagDiv(["nor-white-top", "nor-white-middle", "nor-blue-top", "nor-blue-middle"]);
-
-	// Block object. Object that hides the flag.
-	var Element = {
-		"tagName": "",
-		"classes": [],
-		"id": "",
-		"content": "",
-
-		"init": function (tagName, classlist, id, content) {
-			this.classes = classlist;
-			this.id = id;
-			this.content = content;
-		},
-
-		"getElement": function () {
-			return document.getElementById(this.id);
-		},
-
-		/*
-		* Function that draws the element.
-		*/
-		"draw": function (parentNode) {
-			if (this.tagName !== "") {
-				var elem = "";
-				var check = this.getElement();
-
-				console.log(check);
-
-				if (check === null) {
-					elem = document.createElement(this.tagName);
-				}
-				else {
-					elem = document.createElement(this.tagName);
-					check.parentElement.replaceChild(elem, check);
-				}
-
-				if (this.classes.length > 0) {
-					this.classes.forEach(function (currentValue, index, array) {
-						elem.classList.add(array[index]);
-					});
-				}
-
-				if (this.blockId !== "") {
-					elem.id = this.id;
-				}
-
-				if (this.content !== "") {
-					elem.innerHTML = this.content;
-				}
-
-				if (parentNode !== "") {
-					var elemParent = document.querySelector(parentNode);
-					elemParent.appendChild(elem);
-				}
-			}
-			else {
-				console.log("ERROR: Can't draw element without tagName.");
-			}
-		},
-	};
-
 	// All useful functions.
 	/*
 	In 1964, Richard Durstenfeld came up with the modern method as a computer algorithm. It has a run time complexity of O(n).
@@ -97,38 +19,35 @@ window.Memory = (function(){
 		}
 	}
 
-	// Create all flag objects.
-	// (tagName, classlist=[], id, content="")
-	var gerFlag = Object.create(Element);
-	gerFlag.init("div", ["flag", "germany"], "germany", germanFlagContent);
-	var gerFlag1 = Object.create(Element);
-	gerFlag1.init("div", ["flag", "germany"], "germany1", germanFlagContent);
+	function createFlagDiv(classes) {
+		var parentNode = window.Elemu.create("div", {classList: [className[0]]});
+		// Remove the first class index since that's the parent.
+		classes.shift();
 
-	var jamFlag = Object.create(Element);
-	jamFlag.init("div", ["flag", "jamaica"], "jamaica", jamaicaFlagContent);
-	var jamFlag1 = Object.create(Element);
-	jamFlag1.init("div", ["flag", "jamaica"], "jamaica1", jamaicaFlagContent);
+		// Apply all other classes children of parent.
+		forEach(function (item) {
+			parentNode.appendChild(window.Elemu.create("div", {classList: [item]}));
+		});
+	}
 
-	var sweFlag = Object.create(Element);
-	sweFlag.init("div", ["flag", "sweden"], "sweden", swedishFlagContent);
-	var sweFlag1 = Object.create(Element);
-	sweFlag1.init("div", ["flag", "sweden"], "sweden1", swedishFlagContent);
+	// HTML code for flags.
+	var germanFlag = createFlagDiv(["german", "ger-black-top", "ger-red-middle"]);
+	var jamaicaFlag = createFlagDiv(["jamaica", "jam-triangle-yellow-right", "jam-triangle-yellow-left", "jam-triangle-black-left", "jam-triangle-black-right"]);
+	var swedishFlag = createFlagDiv(["sweden", "swe-yellow-top", "swe-yellow-middle"]);
+	var southAfricanFlag = createFlagDiv(["sa-square-red-top", "sa-square-white-middle", "sa-square-green-middle", "sa-triangle-white", "sa-triangle-green", "sa-triangle-yellow", "sa-triangle-black"]);
+	var norwegianFlag = createFlagDiv(["nor-white-top", "nor-white-middle", "nor-blue-top", "nor-blue-middle"]);
 
-	var saFlag = Object.create(Element);
-	saFlag.init("div", ["flag", "southafrica"], "southafrica", southAfricanFlagContent);
-	var saFlag1 = Object.create(Element);
-	saFlag1.init("div", ["flag", "southafrica"], "southafrica1", southAfricanFlagContent);
-
-	var norFlag = Object.create(Element);
-	norFlag.init("div", ["flag", "norway"], "norway", norwegianFlagContent);
-	var norFlag1 = Object.create(Element);
-	norFlag1.init("div", ["flag", "norway"], "norway1", norwegianFlagContent);
+	var flags = [germanFlag, jamaicaFlag, swedishFlag, southAfricanFlag, norwegianFlag];
+	var flagDuplicates = arr1;
 
 	var Memory = {
 		// Create the array with the flag objects.
-		"myFlags": [gerFlag, jamFlag, sweFlag, saFlag, norFlag, gerFlag1, jamFlag1, sweFlag1, saFlag1, norFlag1],
+		"myFlags": flags.concat(flagDuplicates);
+
 		// List of selected elements.
 		"selected": [],
+
+		// Function to be called when all memory blocks have been revealed.
 		"myCallback": function () {
 			console.log("Called empty callback");
 		},
